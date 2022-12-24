8ca9d4b5b36ce602c4c3ef84a2b439523ff348ae
luytrfish <114283770+luytrfish@users.noreply.github.com>
2022-12-24 12:37:40 +0000

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+UUID=45960ed2-715c-4986-8066-5c1bfb673fcd
+PATHM=lasafd
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8080,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "$UUID",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/$UUID-vmess"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray  run -config=/etc/v2ray/config.json
